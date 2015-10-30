238e9182ae5d3a549538a133b484ebd1e4cea08c
leedu708 <djl265@cornell.edu>
2015-10-29 22:11:31 -0400

    setup cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,21 @@ class User < ActiveRecord::Base
   validates :password,
             :length => { :in => 5..24 },
             :allow_nil => false
+
+  def generate_token
+
+    begin
+      self[:auth_token] = SecureRandom.urlsafe_base64
+    end while User.exists?(:auth_token => self[:auth_token])
+
+  end
+
+  def regenerate_auth_token
+
+    self.auth_token = nil
+    generate_token
+    save!
+
+  end
             
 end
